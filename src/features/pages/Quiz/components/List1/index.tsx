1898a978061116af3a24f37ec6a9002205b7f1e7
GuiHQz <guilher.queiroz@gmail.com>
2023-01-30 22:12:03 -0300

    feat: caixas de seleção do quiz

--- src/features/pages/Quiz/components/List1/index.tsx
+++ src/features/pages/Quiz/components/List1/index.tsx
@@ -3,7 +3,20 @@ import * as Styles from "./styles";
 export const List1: React.FC = () => {
   return (
     <Styles.Container>
-      <h1>Title</h1>
+      <Styles.Row>
+        <Styles.Box>1</Styles.Box>
+        <Styles.Box>2</Styles.Box>
+        <Styles.Box>3</Styles.Box>
+        <Styles.Box>4</Styles.Box>
+        <Styles.Box>5</Styles.Box>
+      </Styles.Row>
+      <Styles.Row>
+        <Styles.Box>6</Styles.Box>
+        <Styles.Box>7</Styles.Box>
+        <Styles.Box>8</Styles.Box>
+        <Styles.Box>9</Styles.Box>
+        <Styles.Box>10</Styles.Box>
+      </Styles.Row>
     </Styles.Container>
   )
 }
--- src/features/pages/Quiz/components/List1/styles.ts
+++ src/features/pages/Quiz/components/List1/styles.ts
@@ -1,5 +1,34 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    color: white;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  margin-bottom: 100px;
+  gap: 150px;
+`
+
+export const Box = styled.div`
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  font-family: 'Roboto', sans-serif;
+  color: black;
+  background-color: gray;
+  gap: 10px;
+  width: 125px;
+  height: 125px;
+  font-size: 40px;
+  font-weight: 700;
+  :hover {
+    background-color: white;
+  }
 `
