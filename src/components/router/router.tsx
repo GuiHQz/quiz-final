c87939aa9c2dab122f592b19a66f78b8033eabda
GuiHQz <guilher.queiroz@gmail.com>
2023-06-04 13:06:35 -0300

    feat: nova rota

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 import Home from "../../features/pages/Home";
 import Quiz from "../../features/pages/Quiz";
+import AllChampions from "../../features/pages/AllChampions/AllChampions";
 
 function Router() {
   return (
@@ -10,13 +11,13 @@ function Router() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/quiz" element={<Quiz />} />
-        <Route path="/table" element={<>página da Tabela</>} />
+        <Route path="/champions" element={<AllChampions />} />
         <Route path="/rules" element={<>página das Regras </>} />
         <Route path="/about-me" element={<>página do About me</>} />
         <Route path="/participants" element={<>página dos Participantes</>} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default Router;
